da7d4f9a1df602bbf56373d7164f0360f79d7770
adyu1121 <adyu1121@gmail.com>
2025-03-12 12:23:27 +0900

    퀴리 수정

--- Discord-for-Langshungjwak/Database.cs
+++ Discord-for-Langshungjwak/Database.cs
@@ -163,14 +163,14 @@ public static class Database
     /// <param name="msg"></param>
     public static void Log(Guid guid, string msg)
     {
-        string q = "insert into log(message, data, time, guid) VALUES(?message, ?data, ?time, ?guid)";
+        string q = "insert into log(message, data, time, guid) VALUES(?message, ?data, Now(), ?guid)";
         try
         {
             using (MySqlCommand cmd = new MySqlCommand(q, client))
             {
                 cmd.Parameters.Add("?message", MySqlDbType.VarString).Value = msg;
                 cmd.Parameters.Add("?data", MySqlDbType.Date).Value = DateTime.Now.Date;
-                cmd.Parameters.Add("?time", MySqlDbType.Timestamp).Value = DateTime.Now;
+                //cmd.Parameters.Add("?time", MySqlDbType.Timestamp).Value = DateTime.Now;
                 cmd.Parameters.Add("?guid", MySqlDbType.Guid).Value = guid;
                 if (!(cmd.ExecuteNonQuery() > 0))
                 {
